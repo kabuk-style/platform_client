058a75fe9f46bdb504685366b3b73b3824423124
Amit Patel <amit@kabuk.com>
2024-12-26 15:52:04 +0530

    refactor: Enhance content API response objects (#3)
    Includes methods to fetch data and pagination details

--- lib/platform_client/responses/base.rb
+++ lib/platform_client/responses/base.rb
@@ -4,7 +4,7 @@ require 'platform_client/errors'
 
 module PlatformClient
   module Responses
-    # Base class for the responses from PlatformClient Platform API
+    # Base class for the responses from Kabuk Platform API
     class Base
       include ActiveModel::Model
       include ActiveModel::Attributes
@@ -12,7 +12,7 @@ module PlatformClient
       attr_reader :result, :original_response
       alias data result
 
-      # @param response [Faraday::Response] the response from the PlatformClient Platform API
+      # @param response [Faraday::Response] the response from the Kabuk Platform API
       def initialize(response)
         @original_response = response
         @result = response.body
--- lib/platform_client/responses/room_categories.rb
+++ lib/platform_client/responses/room_categories.rb
@@ -2,7 +2,13 @@
 
 module PlatformClient
   module Responses
+    # Represents the response from the Kabuk Platform API for the /api/room_categories endpoint
     class RoomCategories < Base
+      include Contentable
+
+      private
+
+      def root_key ='room_categories'
     end
   end
 end
