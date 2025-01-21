1c4a246a5eb1ba38decc3ea80e738791a4533d39
Sooraj Kumar <64979599+sooraj2@users.noreply.github.com>
2025-01-21 20:01:31 +0500

    feat: wrapper for check rate api

---
+++ lib/platform_client/requests/rate.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module PlatformClient
+  DEFAULT_LANGUAGE = 'en-US'
+  JAPANESE_LANGUAGE = 'ja-JP'
+  SUPPORTED_LANGUAGES = [DEFAULT_LANGUAGE, JAPANESE_LANGUAGE].freeze
+
+  module Requests
+    # Wrapper for the /api/check_rate endpoint
+    class Rate < Base
+      attribute :property_code, :string
+      attribute :room_code, :string
+      attribute :check_in_date, :string
+      attribute :check_out_date, :string
+      attribute :adults_count, :integer
+
+      validates :property_code, :room_code, presence: true
+      validates :check_in_date, :check_out_date, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: 'must be in YYYY-MM-DD format' }
+      validates :adults_count, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }, allow_nil: true
+
+      private
+
+      def params
+        attributes.compact_blank
+      end
+    end
+  end
+end
--- spec/platform_client/requests/end_point_spec.rb
+++ spec/platform_client/requests/end_point_spec.rb
@@ -49,5 +49,13 @@ RSpec.describe PlatformClient::Requests::EndPoint do
       expect(endpoint.method).to eq(:get)
       expect(endpoint.type).to eq(:content)
     end
+
+    it 'returns the correct endpoint for rate' do
+      endpoint = described_class.find!(:rate)
+
+      expect(endpoint.uri).to eq('/api/check_rate')
+      expect(endpoint.method).to eq(:get)
+      expect(endpoint.type).to eq(:shopping)
+    end
   end
 end
